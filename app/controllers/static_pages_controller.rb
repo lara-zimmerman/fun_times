06d260a85e9aed9abffe0c9128a880b1bb5291ea
Lara Zimmerman <lara.zimmerman@mail.mcgill.ca>
2017-11-29 17:49:01 -0500

    created static pages controller and three views and two routes

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,7 @@
+class StaticPagesController < ApplicationController
+	def index
+		@state = "new york"
+	end
+	def about_us
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'static_pages#index'
+  get 'about-us', to: 'static_pages#about_us'
+  get 'faq', to: 'static_pages#faq'
 end
